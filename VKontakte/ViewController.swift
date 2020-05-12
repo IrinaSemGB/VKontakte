9e0af1fbcb38ce4fe701f10cd46b8b1f79a425b1
IrinaSemGB <irina.semyachkina@mail.ru>
2020-05-12 14:03:08 +0300

    Initial Commit

---
+++ VKontakte/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VKontakte
+//
+//  Created by Ирина Семячкина on 12.05.2020.
+//  Copyright © 2020 Ирина Семячкина. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
